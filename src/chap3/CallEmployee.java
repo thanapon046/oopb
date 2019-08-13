6e82c7444a07ec45dc356de4a2958ff127c5c2bc
papercut <papercut@172.17.61.171>
2019-08-13 16:27:04 +0700

    1commit

--- src/chap3/CallEmployee.java
+++ src/chap3/CallEmployee.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package chap3;
 
-/**
- *
- * @author papercut
- */
+package chap3;
 public class CallEmployee {
+    public static void main(String[]args){
+        //call static attr & method
+        System.out.println("Id" +Employee.id);
+        Employee.work();
+        
+        //call non/static
+        Employee em = new Employee();
+        System.out.println("Name : "+em.name);
+         System.out.println("Age : "+em.age);
+         em.getSalary();
+    } 
     
 }
